e6b06c3ed2d19be3680f7607ff49f4fec4022b6c
Rasul <rasul.zeynalov.95@gmail.com>
2022-03-29 16:41:16 +0400

    addteacher and addstudent created

--- src/components/student/AddStudent.js
+++ src/components/student/AddStudent.js
@@ -1,11 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Form,Button, Input} from 'reactstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUserPlus} from '@fortawesome/free-solid-svg-icons';
 
 class AddStudent extends Component {
   render() {
     return (
-      <div>
-        AddStudent form
+      <section>
+    <div className="container">
+      <div className="d-flex justify-content-center align-items-center h-100">
+        <div className="col-12 col-md-9 col-lg-7 col-xl-6">
+          <div className="card" style={{borderRadius: "15px"}}>
+            <div className="card-body p-5">
+              <h2 className="text-center mb-5">Şagird əlavə et</h2>
+              <Form>
+                <div className="form-group form-outline mb-4 d-flex justify-content-between">
+                  <Input type="text" className="form-control form-control-lg mr-2" placeholder='Ad'/>
+                  <Input type="text" className="form-control form-control-lg " placeholder='Soyad'/>
+                </div>
+                <div className="form-outline mb-4">
+                  <Input type="text" className="form-control form-control-lg" placeholder='Ata adı'/>
+                </div>
+                <div className="form-outline mb-4">
+                  <Input type="email"  className="form-control form-control-lg" placeholder="Mail" />
+                </div>
+                <select className="custom-select mb-4" required > 
+                      <option selected>Sinif</option>
+                      <option value="0">0</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                </select>
+                <div className="form-outline mb-4">
+                  <Input type="password"  className="form-control form-control-lg" placeholder='Şifrə' />
+                </div>
+                <div className="form-outline mb-4">
+                  <Input type="password"  className="form-control form-control-lg" placeholder='Şifrəni təkrarla' />
+                </div>
+                <div className="d-flex justify-content-center">
+                  <Button type="button" className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">
+                  <FontAwesomeIcon icon={faUserPlus}  size="0.2x" className=" mt-2 mr-2"/>Əlavə et</Button>
+                  </div>
+              </Form>
+
+            </div>
+          </div>
+        </div>
       </div>
+    </div>
+</section>
     )
   }
 }
--- src/components/teacher/AddTeacher.js
+++ src/components/teacher/AddTeacher.js
@@ -1,55 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Form,Button, Input} from 'reactstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faUserPlus} from '@fortawesome/free-solid-svg-icons';
 
 class AddTeacher extends Component {
   render() {
     return (
-      <section className="vh-100 bg-image">
-  <div className="mask d-flex align-items-center h-100 gradient-custom-3">
-    <div className="container h-100">
-      <div className="row d-flex justify-content-center align-items-center h-100">
+      <section>
+    <div className="container">
+      <div className="d-flex justify-content-center align-items-center h-100">
         <div className="col-12 col-md-9 col-lg-7 col-xl-6">
           <div className="card" style={{borderRadius: "15px"}}>
             <div className="card-body p-5">
-              <h2 className="text-uppercase text-center mb-5">Create an account</h2>
-
-              <form>
-
-                <div className="form-outline mb-4">
-                  <input type="text" id="form3Example1cg" className="form-control form-control-lg" />
-                  <label className="form-label" for="form3Example1cg">Your Name</label>
+              <h2 className="text-center mb-5">Müəllim əlavə et</h2>
+              <Form>
+                <div className="form-group form-outline mb-4 d-flex justify-content-between">
+                  <Input type="text" className="form-control form-control-lg mr-2" placeholder='Ad'/>
+                  <Input type="text" className="form-control form-control-lg " placeholder='Soyad'/>
                 </div>
-
                 <div className="form-outline mb-4">
-                  <input type="email" id="form3Example3cg" className="form-control form-control-lg" />
-                  <label className="form-label" for="form3Example3cg">Your Email</label>
+                  <Input type="text" className="form-control form-control-lg" placeholder='Ata adı'/>
                 </div>
-
                 <div className="form-outline mb-4">
-                  <input type="password" id="form3Example4cg" className="form-control form-control-lg" />
-                  <label className="form-label" for="form3Example4cg">Password</label>
+                  <Input type="email"  className="form-control form-control-lg" placeholder="Mail" />
                 </div>
-
+                <select className="custom-select mb-4" required > 
+                      <option selected>Tədris etdiyi fənn</option>
+                      <option value="0">Riyaziyyat</option>
+                      <option value="1">Azərbaycan dili</option>
+                      <option value="2">İngilis dili</option>
+                      <option value="3">Tarix</option>
+                </select>
                 <div className="form-outline mb-4">
-                  <input type="password" id="form3Example4cdg" className="form-control form-control-lg" />
-                  <label className="form-label" for="form3Example4cdg">Repeat your password</label>
+                  <Input type="password"  className="form-control form-control-lg" placeholder='Şifrə' />
                 </div>
-
-                
-
+                <div className="form-outline mb-4">
+                  <Input type="password"  className="form-control form-control-lg" placeholder='Şifrəni təkrarla' />
+                </div>
                 <div className="d-flex justify-content-center">
-                  <button type="button" className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">Register</button>
-                </div>
-
-                <p className="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!" className="fw-bold text-body"><u>Login here</u></a></p>
-
-              </form>
+                  <Button type="button" className="btn btn-success btn-block btn-lg gradient-custom-4 text-body">
+                  <FontAwesomeIcon icon={faUserPlus}  size="0.2x" className=" mt-2 mr-2"/>Əlavə et</Button>
+                  </div>
+              </Form>
 
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
 </section>
     )
   }
