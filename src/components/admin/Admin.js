85cad27cb5f68fc2265043ca4c7bab6d0a0f746d
Rasul <rasul.zeynalov.95@gmail.com>
2022-04-15 17:06:39 +0400

    sdfsd

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,32 +1,69 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { HashRouter,NavLink } from 'react-router-dom';
-import { Card, CardTitle,Row } from 'reactstrap';
+import { Card, CardSubtitle, CardTitle,Row } from 'reactstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {
   faUserPlus,faAddressBook,faMessage,faUserLock,faListCheck
 } from '@fortawesome/free-solid-svg-icons';
 import "../../assets/scss/_main.scss";
+import axios from 'axios';
 
 
 const Example = (props) => {
+
+  const [dataAdmin,setDataAdmin] = useState([]);
+  const [dataTeacher,setDataTeacher] = useState([]);
+  const [dataStudent,setDataStudent] = useState([]);
+
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/data")
+    .then ((users) => {
+    const user=users.data.filter((data) => data.position === "Admin")
+       setDataAdmin(user)
+    })
+  },[]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/data")
+    .then ((users) => {
+    const user=users.data.filter((data) => data.position === "Muellim")
+       setDataTeacher(user)
+    })
+  },[]);
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/data")
+    .then ((users) => {
+    const user=users.data.filter((data) => data.position === "sagird")
+       setDataStudent(user)
+    })
+  },[]);
+  
   return (
     <HashRouter>
     <Row className='cards p-3'>
     <Card body inverse color="success" className='add-user card'>
-        <CardTitle>İstifadəçi əlavə</CardTitle>
+        <CardTitle>İstifadəçi əlavə et</CardTitle>
         <NavLink exact className="nav-link" to="/register" ><FontAwesomeIcon className='icon' icon={faUserPlus} /></NavLink>
       </Card>
     <Card body inverse color="primary" className='admin card' >
         <CardTitle>Admin</CardTitle>
-        <NavLink exact className="nav-link" to="/adminList" ><FontAwesomeIcon className='icon' icon={faUserLock} /></NavLink>
+        <NavLink exact className="nav-link" to="/adminList" ><FontAwesomeIcon className='icon' icon={faUserLock}/>
+        <CardSubtitle className='card-text'>{dataAdmin.length}</CardSubtitle>
+        </NavLink>
       </Card>
       <Card body inverse color="warning" className='teacher-list card' >
         <CardTitle>Müəllim</CardTitle>
-        <NavLink exact className="nav-link" to="/teacherList" ><FontAwesomeIcon className='icon' icon={faAddressBook} /></NavLink>
+        <NavLink exact className="nav-link" to="/teacherList" ><FontAwesomeIcon className='icon' icon={faAddressBook} />
+        <CardSubtitle className='card-text'>{dataTeacher.length}</CardSubtitle>
+        </NavLink>
       </Card>
       <Card body inverse color="warning" className='student-list card'>
         <CardTitle>Şagird</CardTitle>
-        <NavLink exact className="nav-link" to="/studentList" ><FontAwesomeIcon className='icon' icon={faAddressBook} /></NavLink>
+        <NavLink exact className="nav-link" to="/studentList" ><FontAwesomeIcon className='icon' icon={faAddressBook} />
+        <CardSubtitle className='card-text'>{dataStudent.length}</CardSubtitle>
+        </NavLink>
       </Card>
       <Card body inverse color="info" className='add-student card'>
         <CardTitle>Tapşırıqlar</CardTitle>
